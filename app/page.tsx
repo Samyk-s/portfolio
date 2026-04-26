829d1cd3c74763e20563dd58b91f6199ce90478d
Samyk-s <samykbajr919@gmail.com>
2026-04-27 03:46:41 +0545

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,3 @@
-import AboutPage from "./about/page";
-import TechOrbit from "./how/page";
-import Projects from "./projects/page";
-import Contact from "./contact/page";
-import NavbarComp from "./navbar/page";
-import Home from "./Home/page";
-
 export default function Mainpage() {
-  return (
-    <>
-      <NavbarComp />
-      <Home />
-
-      <div className="flex flex-col md:flex-row">
-        <div className="w-full md:flex-1" id="about">
-          <AboutPage />
-        </div>
-        <div className="w-full md:flex-1" id="how">
-          <TechOrbit />
-        </div>
-      </div>
-      <div id="projects">
-        <Projects />
-      </div>
-      <div id="contact">
-        <Contact />
-      </div>
-    </>
-  );
+  return <div>This is demo app</div>;
 }
